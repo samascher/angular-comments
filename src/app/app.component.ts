a470d37880259bc449aae18f809a6155203dd3ee
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-08-15 16:08:39 -0600

    everything but delete and edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+class quote {
+	text: string;
+	author: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
+	  newQuote:quote;
+	  constructor(){
+	  	this.newQuote = new quote();
+	  }
+
+	  comments = [
+      {text:'first comment!', author: 'unknown'},
+      {text: 'nice work!', author: 'unknown'},
+      {text: 'I would also like to congratulate you!', author: 'unknown'}
   ];
+
+	onSubmit(text){
+		if(this.newQuote){
+			var entry = {
+				'text': this.newQuote.text,
+				'author': this.newQuote.author
+			};
+		this.comments.push(entry);
+		}	
+	}
+
+  remove(index: number){
+      this.comments.splice(index, 1);
+    }
 }
